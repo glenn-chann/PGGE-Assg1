42bb47ff403374f547297f5cda4973ae4672b58c
glenn-chann <glenn.chan1212@gmail.com>
2023-11-27 22:41:35 +0800

    finished Question 1

--- Assignment1_UnityProject/Assets/Scripts/TPCBase.cs
+++ Assignment1_UnityProject/Assets/Scripts/TPCBase.cs
@@ -51,7 +51,8 @@ namespace PGGE
             //-------------------------------------------------------------------
 
             //creating varibles for the raycast
-            //origin of the ray will be the player's position + 2 units on the y axis so the start of the ray will be at the head of the character instead of his feet
+            //origin of the ray will be the player's position + 2 units on the y axis so the start of the ray will be at the
+            //head of the character instead of his feet
             rayStart = mPlayerTransform.position + (2 * Vector3.up);
 
             //direction of the ray will be the ((camera's position - player's position) - 2 units on the y axis ).normalised
@@ -60,15 +61,23 @@ namespace PGGE
             rayDir = ((mCameraTransform.position - mPlayerTransform.position) - (2 * Vector3.up)).normalized;
 
             //distance of the ray will be the magnitude of the vector camera's position - player's position
-            rayDistance = (mCameraTransform.position - mPlayerTransform.position).magnitude;
+            rayDistance = (mCameraTransform.position - mPlayerTransform.position).magnitude * 1.2f;
 
             //if the raycast hit an object on the opaque layermask
             if (Physics.Raycast(rayStart, rayDir, out hit, rayDistance, LayerMask.GetMask("Opaque")))
             {
                 Debug.DrawRay(rayStart, rayDir * rayDistance, Color.red, 0.01f);
                 Debug.Log("blocking");
-                //set camera position to the hit.point which is the point the raycast hit the wall - a new vector of the camera position to the player position.normalised * 0.5f to offset the camera off the wall slightly so that it wont clip through the walls at smaller angles
-                mCameraTransform.position = hit.point - ((new Vector3((mCameraTransform.position.x - mPlayerTransform.position.x), mCameraTransform.position.y - (mPlayerTransform.position.y + 2), (mCameraTransform.position.z - mPlayerTransform.position.z)).normalized)*0.5f);
+                //set camera position to the hit.point which is the point the raycast hit the wall -
+                //rayDir * 0.5f to offset the camera off the wall slightly so that it wont clip through
+                //the walls at smaller angles. i used rayDir * 0.5 because its convinently a small value
+                //and we want to offset the camera towards the player.  
+                mCameraTransform.position = hit.point - rayDir * 0.5f;
+                    
+                    
+                    ////((new Vector3((mCameraTransform.position.x 
+                    //- mPlayerTransform.position.x), mCameraTransform.position.y - (mPlayerTransform.position.y + 2), 
+                    //(mCameraTransform.position.z - mPlayerTransform.position.z)).normalized)*0.5f);
             }
             //if the raycast didnt hit anything
             else
